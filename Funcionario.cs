258293d023da6a5e709176fce2a59cab8e3c9b1b
Jorge-Marcelo <jorge3258@hotmail.com>
2024-05-10 17:57:03 -0300

    Primeiro Commit

---
+++ Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Calculo_Salario_POO
+{
+ internal class Funcionario
+ {
+public string Nome{ get; set; }
+public double SalarioBruto { get; set; }
+public double Imposto { get; set; }
+
+
+  public double SalarioLiquido() { return SalarioBruto - Imposto; }
+
+public void AumentoSalario(double porcentagem){
+   SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+  }
+
+  public override string ToString()
+  {
+   return $"{Nome} R$ {SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}";
+  }
+
+ }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Calculo_Salario_POO
+{
+ internal class Program
+ {
+  static void Main(string[] args)
+  {
+Funcionario func = new Funcionario();
+
+   Console.WriteLine("Entre com as Informações do funcionario");
+   Console.Write("Nome: ");
+   func.Nome = Console.ReadLine();
+   Console.Write("Salario bruto: ");
+   func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+   Console.Write("Imposto: ");
+   func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+   Console.WriteLine("-----------------------------------------");
+
+   Console.WriteLine($"Funcionario:  {func}");
+   Console.WriteLine("-----------------------------------------");
+
+   Console.Write("Deseka aumentar o salario em qual porcentagem: ");
+   double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+   func.AumentoSalario(porcent);
+   Console.WriteLine("-----------------------------------------");
+
+   Console.WriteLine($"Dados Atualizados:  {func} ");
+
+
+
+
+  }
+ }
+}
